131a976e3f90bc6c3b9f1a970eb22f06568797b7
yewon.seo <dodnjs1241@naver.com>
2023-02-19 16:54:06 +0900

    🧑🏻‍💻feat: baseEntity 생성
    - 생성시간을 필드로 가지고 있는 baseEntity 를 생성하였습니다

---
+++ src/main/java/shop/zip/travel/global/entity/BaseTimeEntity.java
@@ -0,0 +1,21 @@
+package shop.zip.travel.global.entity;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+
+	@CreatedDate
+	private LocalDateTime createDate;
+
+	public LocalDateTime getCreateDate() {
+		return createDate;
+	}
+}
