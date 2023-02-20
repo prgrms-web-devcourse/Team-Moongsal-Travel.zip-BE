e698b141f5cb973ed74516230669b82216d76c3d
yewon.seo <dodnjs1241@naver.com>
2023-02-20 21:42:04 +0900

    🧑🏻‍💻feat: title 컬럼 길이 제한 추가

--- src/main/java/shop/zip/travel/domain/travelog/entity/SubTravelogue.java
+++ src/main/java/shop/zip/travel/domain/travelog/entity/SubTravelogue.java
@@ -19,7 +19,7 @@ public class SubTravelogue {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@Column(nullable = false)
+	@Column(nullable = false, length = 50)
 	private String title;
 
 	@Column(columnDefinition = "LONGTEXT", nullable = false)
