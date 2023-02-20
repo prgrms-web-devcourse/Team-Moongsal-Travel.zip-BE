ed2d3ef4d7eeca8fb33659501bfe5e505d78cb77
Albatross3 <superstring77@gmail.com>
2023-02-21 00:05:24 +0900

    🧑🏻‍💻feat : Member 엔티티 validation 추가
    - Member 엔티티 생성시 email, password, nickname 에 대해서 검증 로직을 추가하였습니다

--- src/main/java/shop/zip/travel/domain/member/entity/Member.java
+++ src/main/java/shop/zip/travel/domain/member/entity/Member.java
@@ -1,6 +1,11 @@
 package shop.zip.travel.domain.member.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import org.springframework.util.Assert;
 import shop.zip.travel.global.entity.BaseTimeEntity;
 
 
@@ -28,16 +33,36 @@ public class Member extends BaseTimeEntity {
   }
 
   public Member(String email, String password, String nickname) {
+    validateEmail(email);
+    validatePassword(password);
+    validateNickname(nickname);
     this.email = email;
     this.password = password;
     this.nickname = nickname;
   }
+
 
   public Member(String email, String password, String nickname, int birthYear) {
     this.email = email;
     this.password = password;
     this.nickname = nickname;
     this.birthYear = birthYear;
+  }
+
+
+  private void validateEmail(String email) {
+    String emailPattern = "^[\\w-.]+@[\\w-]+.[\\w.]+$";
+    Assert.isTrue(email.matches(emailPattern),"이메일이 형식에 맞지 않습니다");
+  }
+
+  private void validatePassword(String password) {
+    String passwordPattern = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[~!@#$%^&*])[A-Za-z\\d~!@#$%^&*]{8,}$";
+    Assert.isTrue(password.matches(passwordPattern),"비밀번호가 형식에 맞지 않습니다");
+  }
+
+  private void validateNickname(String nickname) {
+    String nicknamePattern = "^[가-힣|a-zA-Z]{2,12}$";
+    Assert.isTrue(nickname.matches(nicknamePattern),"닉네임이 형식에 맞지 않습니다");
   }
 
   public Long getId() {
