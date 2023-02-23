9fe5672233f6596ac60ca78bf65984429d863453
cloudwi <cloudwi@naver.com>
2023-02-23 17:42:45 +0900

    fix : TravelogueRepositoryImpl Bean 주입 설정 추가

--- src/main/java/shop/zip/travel/domain/post/travelogue/repository/impl/TravelogueRepositoryImpl.java
+++ src/main/java/shop/zip/travel/domain/post/travelogue/repository/impl/TravelogueRepositoryImpl.java
@@ -1,21 +1,11 @@
 package shop.zip.travel.domain.post.travelogue.repository.impl;
 
-import static shop.zip.travel.domain.post.travelogue.entity.QTravelogue.travelogue;
-import static shop.zip.travel.domain.member.entity.QMember.member;
-import static shop.zip.travel.domain.post.subTravelogue.entity.QSubTravelogue.subTravelogue;
-
-import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import java.util.List;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Slice;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 import org.springframework.stereotype.Repository;
-import shop.zip.travel.domain.member.entity.QMember;
-import shop.zip.travel.domain.post.travelogue.dto.TravelogueSearchRes;
 import shop.zip.travel.domain.post.travelogue.dto.TravelogueSimpleRes;
-import shop.zip.travel.domain.post.travelogue.entity.QTravelogue;
 import shop.zip.travel.domain.post.travelogue.entity.Travelogue;
 import shop.zip.travel.domain.post.travelogue.repository.querydsl.TravelogueRepositoryQuerydsl;
 
