87c3dac249fbce470776913a7718c007d564dfe2
cloudwi <cloudwi@naver.com>
2023-02-23 22:05:14 +0900

    🧑🏻‍💻feat : querydsl 검색 개발
    - Projections 사용하여 성능 최적화
    - 패치 조인으로 n + 1문제 제거

--- src/main/java/shop/zip/travel/domain/post/travelogue/repository/TravelogueRepository.java
+++ src/main/java/shop/zip/travel/domain/post/travelogue/repository/TravelogueRepository.java
@@ -3,8 +3,10 @@ package shop.zip.travel.domain.post.travelogue.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import shop.zip.travel.domain.post.travelogue.entity.Travelogue;
+import shop.zip.travel.domain.post.travelogue.repository.querydsl.TravelogueRepositoryQuerydsl;
 
 @Repository
-public interface TravelogueRepository extends JpaRepository<Travelogue, Long> {
+public interface TravelogueRepository extends JpaRepository<Travelogue, Long>,
+    TravelogueRepositoryQuerydsl {
 
 }
