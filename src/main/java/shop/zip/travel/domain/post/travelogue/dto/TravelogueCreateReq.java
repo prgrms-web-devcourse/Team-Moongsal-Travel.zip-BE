0859e33c33c4d27a58ce40a8d184f1929d63ef3d
yewon.seo <dodnjs1241@naver.com>
2023-02-23 23:36:29 +0900

    👨🏻‍💻feat: Travelogue 생성 시 필요한 Dto 추가

---
+++ src/main/java/shop/zip/travel/domain/post/travelogue/dto/TravelogueCreateReq.java
@@ -0,0 +1,34 @@
+package shop.zip.travel.domain.post.travelogue.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import shop.zip.travel.domain.member.entity.Member;
+import shop.zip.travel.domain.post.data.Country;
+import shop.zip.travel.domain.post.travelogue.data.Cost;
+import shop.zip.travel.domain.post.travelogue.data.Period;
+import shop.zip.travel.domain.post.travelogue.entity.Travelogue;
+
+public record TravelogueCreateReq(
+    @NotNull
+    Period period,
+    @NotBlank
+    String title,
+    @NotNull
+    Country country,
+    @NotBlank
+    String thumbnail,
+    @NotNull
+    Cost cost
+) {
+
+    public Travelogue toEntity(Member member) {
+        return new Travelogue(
+            this.period,
+            this.title,
+            this.country,
+            this.thumbnail,
+            this.cost,
+            member
+        );
+    }
+}
