f0a17b4458af94d30f325c2dfd1d62f9637ef14b
yewon.seo <dodnjs1241@naver.com>
2023-02-23 23:36:50 +0900

    👨🏻‍💻feat: Travelogue 생성 api개발

--- src/main/java/shop/zip/travel/domain/post/travelogue/service/TravelogueService.java
+++ src/main/java/shop/zip/travel/domain/post/travelogue/service/TravelogueService.java
@@ -1,8 +1,31 @@
 package shop.zip.travel.domain.post.travelogue.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import shop.zip.travel.domain.member.entity.Member;
+import shop.zip.travel.domain.member.service.MemberService;
+import shop.zip.travel.domain.post.travelogue.dto.TravelogueCreateReq;
+import shop.zip.travel.domain.post.travelogue.repository.TravelogueRepository;
 
 @Service
+@Transactional(readOnly = true)
 public class TravelogueService {
+
+	private final TravelogueRepository travelogueRepository;
+	private final MemberService memberService;
+
+	public TravelogueService(TravelogueRepository travelogueRepository,
+		MemberService memberService) {
+		this.travelogueRepository = travelogueRepository;
+		this.memberService = memberService;
+	}
+
+	@Transactional
+	public Long createTravelogue(TravelogueCreateReq createReq, Long memberId) {
+		Member findMember = memberService.getMember(memberId);
+		return travelogueRepository.save(createReq.toEntity(findMember))
+			.getId();
+	}
 
 }
--- src/main/java/shop/zip/travel/presentation/travelogue/TravelogueController.java
+++ src/main/java/shop/zip/travel/presentation/travelogue/TravelogueController.java
@@ -1,10 +1,32 @@
 package shop.zip.travel.presentation.travelogue;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+import shop.zip.travel.domain.post.travelogue.dto.TravelogueCreateReq;
+import shop.zip.travel.domain.post.travelogue.service.TravelogueService;
 
 @RestController
 @RequestMapping("/api/travelogues")
 public class TravelogueController {
+
+	private final TravelogueService travelogueService;
+
+	public TravelogueController(TravelogueService travelogueService) {
+		this.travelogueService = travelogueService;
+	}
+
+	@PostMapping
+	public ResponseEntity<Long> create(@RequestBody @Valid TravelogueCreateReq createReq,
+		@RequestParam Long memberId) {
+		Long travelogueId = travelogueService.createTravelogue(createReq, memberId);
+
+		return ResponseEntity.ok(travelogueId);
+	}
 
 }
