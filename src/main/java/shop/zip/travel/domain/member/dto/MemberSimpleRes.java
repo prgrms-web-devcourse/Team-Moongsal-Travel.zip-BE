0c920718e4ed522f5d2a49b89b32ff68837c0f7a
Surin Lee <leesurin99@gmail.com>
2023-02-23 17:57:49 +0900

    ♻️refactor : DTO 수정
    - dto 내부에 정적 팩토리 메소드 추가

--- src/main/java/shop/zip/travel/domain/member/dto/MemberSimpleRes.java
+++ src/main/java/shop/zip/travel/domain/member/dto/MemberSimpleRes.java
@@ -1,7 +1,16 @@
 package shop.zip.travel.domain.member.dto;
 
+import shop.zip.travel.domain.post.travelogue.dto.TravelogueSimple;
+
 public record MemberSimpleRes(
 	String nickname,
 	String profileImageUrl
 	) {
+
+	public static MemberSimpleRes toDto(TravelogueSimple travelogueSimple){
+		return new MemberSimpleRes(
+			travelogueSimple.memberNickname(),
+			travelogueSimple.memberProfileImageUrl()
+		);
+	}
 }
