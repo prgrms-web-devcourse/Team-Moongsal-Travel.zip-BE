16deb9593a7458a7356f95b33c6ca4978e07e363
Albatross3 <superstring77@gmail.com>
2023-02-24 11:20:07 +0900

    🧑🏻‍💻feat : dto validation 추가
    - 인증 코드 숫자 6자리로 validation 추가하였습니다
    - 탄생년 숫자 4자리로 validation 추가하였습니다

--- src/main/java/shop/zip/travel/domain/email/dto/request/CodeValidateReq.java
+++ src/main/java/shop/zip/travel/domain/email/dto/request/CodeValidateReq.java
@@ -1,5 +1,11 @@
 package shop.zip.travel.domain.email.dto.request;
 
-public record CodeValidateReq(String email, String code) {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+public record CodeValidateReq(
+    @NotBlank @Email String email,
+    @NotBlank @Pattern(regexp = "[0-9]{6}") String code) {
 
 }
