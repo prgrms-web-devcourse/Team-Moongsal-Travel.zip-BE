4fc7b392b7a70a3031fe6ab009e48fef03d90762
yewon.seo <dodnjs1241@naver.com>
2023-02-25 11:17:31 +0900

    👨🏻‍💻feat: 교통수단 중복제거하여 보여주는 필드 추가

--- src/main/java/shop/zip/travel/domain/post/travelogue/dto/res/TravelogueDetailRes.java
+++ src/main/java/shop/zip/travel/domain/post/travelogue/dto/res/TravelogueDetailRes.java
@@ -1,8 +1,13 @@
 package shop.zip.travel.domain.post.travelogue.dto.res;
 
 import java.time.temporal.ChronoUnit;
+import java.util.Collection;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import shop.zip.travel.domain.post.subTravelogue.data.Transportation;
 import shop.zip.travel.domain.post.subTravelogue.dto.SubTravelogueDetailRes;
+import shop.zip.travel.domain.post.subTravelogue.entity.SubTravelogue;
 import shop.zip.travel.domain.post.travelogue.entity.Travelogue;
 
 public record TravelogueDetailRes(
@@ -14,12 +19,14 @@ public record TravelogueDetailRes(
     Long nights,
     Long days,
     Long totalCost,
-    List<SubTravelogueDetailRes> subTravelogues
+    List<SubTravelogueDetailRes> subTravelogues,
+    Set<Transportation> transportations
 ) {
 
     public static TravelogueDetailRes toDto(Travelogue travelogue) {
         long nights = ChronoUnit.DAYS.between(travelogue.getPeriod().getStartDate(),
             travelogue.getPeriod().getEndDate());
+
         return new TravelogueDetailRes(
             travelogue.getMember().getProfileImageUrl(),
             travelogue.getMember().getNickname(),
@@ -32,7 +39,12 @@ public record TravelogueDetailRes(
             travelogue.getSubTravelogues()
                 .stream()
                 .map(SubTravelogueDetailRes::toDto)
-                .toList()
+                .toList(),
+            travelogue.getSubTravelogues()
+                .stream()
+                .map(SubTravelogue::getTransportationSet)
+                .flatMap(Collection::stream)
+                .collect(Collectors.toSet())
         );
     }
 }
