c59ce2e702e6c5a47a4019c6996b5e49cfbd0443
Surin Lee <leesurin99@gmail.com>
2023-02-25 16:31:17 +0900

    👩🏻‍💻feat : 전체 Travelogue 리스트를 불러오는 Service 구현
    - CustomSlice를 사용하여 중복된 값 제거 및 원하는 정보만 전달하도록 변경

--- src/main/java/shop/zip/travel/domain/post/travelogue/service/TravelogueService.java
+++ src/main/java/shop/zip/travel/domain/post/travelogue/service/TravelogueService.java
@@ -1,8 +1,33 @@
 package shop.zip.travel.domain.post.travelogue.service;
 
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import shop.zip.travel.domain.post.travelogue.dto.TravelogueSimple;
+import shop.zip.travel.domain.post.travelogue.dto.res.CustomSlice;
+import shop.zip.travel.domain.post.travelogue.dto.res.TravelogueSimpleRes;
+import shop.zip.travel.domain.post.travelogue.repository.TravelogueRepository;
 
 @Service
+@Transactional(readOnly = true)
 public class TravelogueService {
 
+	private final TravelogueRepository travelogueRepository;
+
+	public TravelogueService(TravelogueRepository travelogueRepository) {
+		this.travelogueRepository = travelogueRepository;
+	}
+
+	public CustomSlice<TravelogueSimpleRes> getTravelogues(int page, int size) {
+		PageRequest pageRequest = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "createDate"));
+
+		Slice<TravelogueSimple> travelogues = travelogueRepository.findAllBySlice(pageRequest);
+
+		return CustomSlice.toDto(
+			travelogues.map(TravelogueSimpleRes::toDto)
+		);
+	}
 }
