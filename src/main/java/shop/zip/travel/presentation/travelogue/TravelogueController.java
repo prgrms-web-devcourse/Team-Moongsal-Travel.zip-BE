d89bdeb8777adfc5d7c85119234cc3f0bb8fcc83
Surin Lee <leesurin99@gmail.com>
2023-02-26 23:36:14 +0900

    ♻️refactor : TravelogueController 수정
    - 모든 게시물 조회 쿼리파라미터 기본값 상수화

--- src/main/java/shop/zip/travel/presentation/travelogue/TravelogueController.java
+++ src/main/java/shop/zip/travel/presentation/travelogue/TravelogueController.java
@@ -1,6 +1,7 @@
 package shop.zip.travel.presentation.travelogue;
 
 import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
 import shop.zip.travel.domain.post.travelogue.dto.req.TravelogueCreateReq;
-import shop.zip.travel.domain.post.travelogue.dto.res.TravelogueSimpleRes;
 import shop.zip.travel.domain.post.travelogue.dto.res.TravelogueCustomSlice;
 import shop.zip.travel.domain.post.travelogue.dto.res.TravelogueSimpleRes;
 import shop.zip.travel.domain.post.travelogue.service.TravelogueService;
@@ -19,6 +19,10 @@ import shop.zip.travel.domain.post.travelogue.service.TravelogueService;
 @RestController
 @RequestMapping("/api/travelogues")
 public class TravelogueController {
+
+	private static final String DEFAULT_SIZE = "5";
+	private static final String DEFAULT_PAGE = "0";
+	private static final String DEFAULT_SORT_FIELD = "createDate";
 
 	private final TravelogueService travelogueService;
 
@@ -28,7 +32,7 @@ public class TravelogueController {
 
 	@PostMapping
 	public ResponseEntity<Long> create(
-			@RequestBody @Valid TravelogueCreateReq createReq,
+		@RequestBody @Valid TravelogueCreateReq createReq,
 			@RequestParam Long memberId) {
 
 		Long travelogueId = travelogueService.save(createReq, memberId);
@@ -37,19 +41,20 @@ public class TravelogueController {
 
 	@GetMapping("/search")
 	public ResponseEntity<List<TravelogueSimpleRes>> search(
-			@RequestParam(name = "keyword", required = false) String keyword,
-			@RequestParam(name = "lastTravelogue", required = false) Long lastTravelogue,
-			@RequestParam(name = "orderType") String orderType, @RequestParam(name = "size") int size) {
+		@RequestParam(name = "keyword", required = false) String keyword,
+		@RequestParam(name = "lastTravelogue", required = false) Long lastTravelogue,
+		@RequestParam(name = "orderType") String orderType, @RequestParam(name = "size") int size) {
 		List<TravelogueSimpleRes> travelogueSimpleResList = travelogueService.search(lastTravelogue,
-				keyword, orderType, size);
+			keyword, orderType, size);
 
 		return ResponseEntity.ok(travelogueSimpleResList);
 	}
+
 	@GetMapping
 	public ResponseEntity<TravelogueCustomSlice<TravelogueSimpleRes>> getAll(
-		@RequestParam(required = false, defaultValue = "5") int size,
-		@RequestParam(required = false, defaultValue = "0") int page,
-		@RequestParam(required = false, defaultValue = "createDate") String sortField) {
+		@RequestParam(required = false, defaultValue = DEFAULT_SIZE) int size,
+		@RequestParam(required = false, defaultValue = DEFAULT_PAGE) int page,
+		@RequestParam(required = false, defaultValue = DEFAULT_SORT_FIELD) String sortField) {
 		TravelogueCustomSlice<TravelogueSimpleRes> travelogueSimpleRes =
 			travelogueService.getTravelogues(page, size, sortField);
 
