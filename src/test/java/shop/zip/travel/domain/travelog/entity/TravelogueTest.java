eba030d06f23843f5005528838037fd519021f0e
Surin Lee <leesurin99@gmail.com>
2023-02-27 16:43:49 +0900

    💯test : 빼먹은 메소드 추가
    - 예외 클래스 인스턴스 검사 메소드 추가

--- src/test/java/shop/zip/travel/domain/travelog/entity/TravelogueTest.java
+++ src/test/java/shop/zip/travel/domain/travelog/entity/TravelogueTest.java
@@ -1,12 +1,10 @@
 package shop.zip.travel.domain.travelog.entity;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
 import shop.zip.travel.domain.post.travelogue.DummyGenerator;
 import shop.zip.travel.domain.post.travelogue.entity.Travelogue;
 
@@ -33,38 +31,38 @@ class TravelogueTest {
 	@DisplayName("기간 없이 메인 게시물을 생성할 수 없다.")
 	void create_fail_by_no_period(){
 		assertThatThrownBy(() -> new Travelogue(
-			null,
-			title,
-			DummyGenerator.createCountry(),
-			thumbnail,
-			DummyGenerator.createCost(),
-			List.of(DummyGenerator.createSubTravelogue()),
-			DummyGenerator.createMember()
-		));
+				null,
+				title,
+				DummyGenerator.createCountry(),
+				thumbnail,
+				DummyGenerator.createCost(),
+				List.of(DummyGenerator.createSubTravelogue()),
+				DummyGenerator.createMember()
+		)).isInstanceOf(IllegalArgumentException.class);
 	}
 
 	@Test
 	@DisplayName("제목 없이 메인 게시물을 생성 할 수 없다.")
 	void create_fail_by_no_title(){
 		assertThatThrownBy(() -> new Travelogue(
-			DummyGenerator.createPeriod(),
-			null,
-			DummyGenerator.createCountry(),
-			thumbnail,
-			DummyGenerator.createCost(),
-			List.of(DummyGenerator.createSubTravelogue()),
-			DummyGenerator.createMember()
-		));
+				DummyGenerator.createPeriod(),
+				null,
+				DummyGenerator.createCountry(),
+				thumbnail,
+				DummyGenerator.createCost(),
+				List.of(DummyGenerator.createSubTravelogue()),
+				DummyGenerator.createMember()
+		)).isInstanceOf(IllegalArgumentException.class);
 
 		assertThatThrownBy(() -> new Travelogue(
-			DummyGenerator.createPeriod(),
-			"",
-			DummyGenerator.createCountry(),
-			thumbnail,
-			DummyGenerator.createCost(),
-			List.of(DummyGenerator.createSubTravelogue()),
-			DummyGenerator.createMember()
-		));
+				DummyGenerator.createPeriod(),
+				"",
+				DummyGenerator.createCountry(),
+				thumbnail,
+				DummyGenerator.createCost(),
+				List.of(DummyGenerator.createSubTravelogue()),
+				DummyGenerator.createMember()
+		)).isInstanceOf(IllegalArgumentException.class);
 	}
 
 	@Test
