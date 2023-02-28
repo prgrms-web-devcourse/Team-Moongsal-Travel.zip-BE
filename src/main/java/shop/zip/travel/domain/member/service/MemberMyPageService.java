a5a42f6d1048088d645f962e0355276f8ec6edb1
yewon.seo <dodnjs1241@naver.com>
2023-02-28 17:14:42 +0900

    👨🏻‍💻feat: 검증로직 추가
    - user가 저장되어있는지 아닌지 확인하는 검증 코드 추가

--- src/main/java/shop/zip/travel/domain/member/service/MemberMyPageService.java
+++ src/main/java/shop/zip/travel/domain/member/service/MemberMyPageService.java
@@ -2,7 +2,9 @@ package shop.zip.travel.domain.member.service;
 
 import org.springframework.stereotype.Service;
 import shop.zip.travel.domain.member.dto.response.MemberInfoRes;
+import shop.zip.travel.domain.member.exception.MemberNotFoundException;
 import shop.zip.travel.domain.member.repository.MemberRepository;
+import shop.zip.travel.global.error.ErrorCode;
 
 @Service
 public class MemberMyPageService {
@@ -13,7 +15,15 @@ public class MemberMyPageService {
     this.memberRepository = memberRepository;
   }
 
+  private void checkMemberExist(Long id) {
+    if (memberRepository.existsById(id)) {
+      return;
+    }
+    throw new MemberNotFoundException(ErrorCode.MEMBER_NOT_FOUND);
+  }
+
   public MemberInfoRes getInfoBy(Long memberId) {
+    checkMemberExist(memberId);
     return memberRepository.getMemberInfo(memberId);
   }
 
