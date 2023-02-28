21d670faef361042d06b46a296762a1eddf2dade
Surin Lee <leesurin99@gmail.com>
2023-03-01 03:37:08 +0900

    ♻️refactor: DTO 내에 객체에 @Valid 추가

--- src/main/java/shop/zip/travel/domain/post/travelogue/dto/req/TravelogueCreateReq.java
+++ src/main/java/shop/zip/travel/domain/post/travelogue/dto/req/TravelogueCreateReq.java
@@ -1,5 +1,6 @@
 package shop.zip.travel.domain.post.travelogue.dto.req;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import shop.zip.travel.domain.member.entity.Member;
@@ -9,27 +10,30 @@ import shop.zip.travel.domain.post.travelogue.data.Period;
 import shop.zip.travel.domain.post.travelogue.entity.Travelogue;
 
 public record TravelogueCreateReq(
-    @NotNull
-    Period period,
-    @NotBlank
-    String title,
-    @NotNull
-    Country country,
-    @NotBlank
-    String thumbnail,
-    @NotNull
-    Cost cost
+  @Valid
+  @NotNull
+  Period period,
+  @NotBlank
+  String title,
+  @Valid
+  @NotNull
+  Country country,
+  @NotBlank
+  String thumbnail,
+  @Valid
+  @NotNull
+  Cost cost
 ) {
 
-    public Travelogue toTravelogue(Member member) {
-        return new Travelogue(
-            this.period,
-            this.title,
-            this.country,
-            this.thumbnail,
-            this.cost,
-            member
-        );
-    }
+  public Travelogue toTravelogue(Member member) {
+    return new Travelogue(
+      this.period,
+      this.title,
+      this.country,
+      this.thumbnail,
+      this.cost,
+      member
+    );
+  }
 }
 
