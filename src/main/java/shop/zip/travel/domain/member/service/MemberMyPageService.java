d5a8be5ca627058c073e33cefc49e0c2f2023316
yewon.seo <dodnjs1241@naver.com>
2023-03-01 00:44:11 +0900

    ♻refactor: 검증 코드 삭제️

--- src/main/java/shop/zip/travel/domain/member/service/MemberMyPageService.java
+++ src/main/java/shop/zip/travel/domain/member/service/MemberMyPageService.java
@@ -6,11 +6,9 @@ import org.springframework.data.domain.SliceImpl;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import shop.zip.travel.domain.member.dto.response.MemberInfoRes;
-import shop.zip.travel.domain.member.exception.MemberNotFoundException;
 import shop.zip.travel.domain.member.repository.MemberRepository;
 import shop.zip.travel.domain.post.travelogue.dto.res.TravelogueSimpleRes;
 import shop.zip.travel.domain.post.travelogue.repository.TravelogueRepository;
-import shop.zip.travel.global.error.ErrorCode;
 
 @Service
 @Transactional(readOnly = true)
@@ -25,15 +23,7 @@ public class MemberMyPageService {
     this.memberRepository = memberRepository;
   }
 
-  private void checkMemberExist(Long id) {
-    if (memberRepository.existsById(id)) {
-      return;
-    }
-    throw new MemberNotFoundException(ErrorCode.MEMBER_NOT_FOUND);
-  }
-
   public MemberInfoRes getInfoBy(Long memberId) {
-    checkMemberExist(memberId);
     return memberRepository.getMemberInfo(memberId);
   }
 
