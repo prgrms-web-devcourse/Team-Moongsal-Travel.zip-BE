4cb1cbe071f484648569960eda922d6ce84a52cc
cloudwi <cloudwi@naver.com>
2023-03-01 01:29:18 +0900

    🔨feat : profile 수정 하였습니다.

--- scripts/run_new_jar.sh
+++ scripts/run_new_jar.sh
@@ -3,20 +3,20 @@
 CURRENT_PORT=$(cat /home/ec2-user/service_url.inc | grep -Po '[0-9]+' | tail -1)
 TARGET_PORT=0
 
-echo "> Current port of running WAS is ${CURRENT_PORT}."
+echo "> Current port of running JAR is ${CURRENT_PORT}."
 
 if [ ${CURRENT_PORT} -eq 8081 ]; then
   TARGET_PORT=8082
 elif [ ${CURRENT_PORT} -eq 8082 ]; then
   TARGET_PORT=8081
 else
-  echo "> No WAS is connected to nginx"
+  echo "> No JAR is connected to nginx"
 fi
 
 TARGET_PID=$(lsof -Fp -i TCP:${TARGET_PORT} | grep -Po 'p[0-9]+' | grep -Po '[0-9]+')
 
 if [ ! -z ${TARGET_PID} ]; then
-  echo "> Kill WAS running at ${TARGET_PORT}."
+  echo "> Kill JAR running at ${TARGET_PORT}."
   sudo kill ${TARGET_PID}
 fi
 
