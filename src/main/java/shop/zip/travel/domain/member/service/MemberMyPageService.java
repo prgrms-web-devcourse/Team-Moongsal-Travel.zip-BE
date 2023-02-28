3d14caf2ee269c25605d1869119b9ede0d250290
yewon.seo <dodnjs1241@naver.com>
2023-02-28 23:05:15 +0900

    👨🏻‍💻feat: transactional 어노테이션 추가

--- src/main/java/shop/zip/travel/domain/member/service/MemberMyPageService.java
+++ src/main/java/shop/zip/travel/domain/member/service/MemberMyPageService.java
@@ -1,12 +1,14 @@
 package shop.zip.travel.domain.member.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import shop.zip.travel.domain.member.dto.response.MemberInfoRes;
 import shop.zip.travel.domain.member.exception.MemberNotFoundException;
 import shop.zip.travel.domain.member.repository.MemberRepository;
 import shop.zip.travel.global.error.ErrorCode;
 
 @Service
+@Transactional(readOnly = true)
 public class MemberMyPageService {
 
   private final MemberRepository memberRepository;
