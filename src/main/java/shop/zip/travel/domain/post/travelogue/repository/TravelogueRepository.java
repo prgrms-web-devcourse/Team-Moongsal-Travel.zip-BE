8a27302efe758f9f855f7eb6466e3ca16ee0ff85
yewon.seo <dodnjs1241@naver.com>
2023-03-01 00:19:12 +0900

    ♻️refactor: TravelogueSimple생성자 Id 추가

--- src/main/java/shop/zip/travel/domain/post/travelogue/repository/TravelogueRepository.java
+++ src/main/java/shop/zip/travel/domain/post/travelogue/repository/TravelogueRepository.java
@@ -22,18 +22,18 @@ public interface TravelogueRepository extends JpaRepository<Travelogue, Long>,
 			+ "on m.id = t.member.id ")
 	Slice<TravelogueSimple> findAllBySlice(@Param("pageRequest") PageRequest pageRequest);
 
-	@Query(value = "select t "
+	@Query("select t "
 		+ "from Travelogue t "
 		+ "left join fetch t.member "
 		+ "where t.id = :travelogueId")
 	Travelogue getTravelogueDetail(@Param("travelogueId") Long travelogueId);
 
   @Query(
-    "select new shop.zip.travel.domain.post.travelogue.dto.TravelogueSimple("
-      + "t.title, t.period, t.cost.total, t.country.name, t.thumbnail, m.nickname, m.profileImageUrl)"
-      + "from Travelogue t "
-      + "inner join t.member m "
-      + "where m.id = :memberId ")
+		"select new shop.zip.travel.domain.post.travelogue.dto.TravelogueSimple("
+			+ "t.id, t.title, t.period, t.cost.total, t.country.name, t.thumbnail, m.nickname, m.profileImageUrl)"
+			+ "from Travelogue t "
+			+ "inner join t.member m "
+			+ "where m.id = :memberId ")
   Slice<TravelogueSimple> getMyTravelogues(@Param("memberId") Long memberId,
     @Param("pageable") Pageable pageable);
 
