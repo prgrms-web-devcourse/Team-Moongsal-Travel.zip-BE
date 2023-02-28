c171796e6d0abec4418e9069fe1c96b4d8a9c1d9
yewon.seo <dodnjs1241@naver.com>
2023-02-28 17:15:04 +0900

    💯test: MemberMyPageControllerTest 추가

---
+++ src/test/java/shop/zip/travel/presentation/member/MemberMyPageControllerTest.java
@@ -0,0 +1,78 @@
+package shop.zip.travel.presentation.member;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+import shop.zip.travel.domain.member.entity.Member;
+import shop.zip.travel.domain.member.repository.MemberRepository;
+import shop.zip.travel.global.security.JwtTokenProvider;
+
+@AutoConfigureMockMvc
+@AutoConfigureRestDocs
+@SpringBootTest
+@Transactional
+@ExtendWith(SpringExtension.class)
+class MemberMyPageControllerTest {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Autowired
+  private MemberRepository memberRepository;
+
+  @Autowired
+  private JwtTokenProvider jwtTokenProvider;
+
+  private final ObjectMapper objectMapper = new ObjectMapper();
+
+  private Member member;
+
+  @BeforeEach
+  void setUp() {
+    member = new Member(
+      "user@naver.com",
+      "password1234!",
+      "nickname",
+      "2000",
+      "ProfileUrlForTest");
+
+    memberRepository.save(member);
+  }
+
+  @DisplayName("유저는 본인의 개인정보를 조회할 수 있다")
+  @Test
+  public void get_my_page_info() throws Exception {
+
+    String token = "Bearer " + jwtTokenProvider.createToken(member.getId());
+
+    mockMvc.perform(get("/api/members/my/info")
+        .header("AccessToken", token))
+      .andExpect(status().isOk())
+      .andDo(print())
+      .andDo(document("get-my-info",
+        responseFields(
+          fieldWithPath("email").description("이메일"),
+          fieldWithPath("nickname").description("닉네임"),
+          fieldWithPath("birthYear").description("생년월일"),
+          fieldWithPath("profileImageUrl").description("프로필 이미지 url")
+        )
+      ));
+  }
+
+}
