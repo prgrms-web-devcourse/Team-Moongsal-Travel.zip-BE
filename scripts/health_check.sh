a2044dc5de1c9f2d293c7b6d1eb4fbb7a8095db4
cloudwi <cloudwi@naver.com>
2023-03-01 01:36:26 +0900

    🔨feat : HealthController 구현

--- scripts/health_check.sh
+++ scripts/health_check.sh
@@ -10,12 +10,12 @@ if [ ${CURRENT_PORT} -eq 8081 ]; then
 elif [ ${CURRENT_PORT} -eq 8082 ]; then
     TARGET_PORT=8081
 else
-    echo "> No WAS is connected to nginx"
+    echo "> No JAR is connected to nginx"
     exit 1
 fi
 
 
-echo "> Start health check of WAS at 'http://127.0.0.1:${TARGET_PORT}' ..."
+echo "> Start health check of JAR at 'http://127.0.0.1:${TARGET_PORT}' ..."
 
 for RETRY_COUNT in 1 2 3 4 5 6 7 8 9 10
 do
@@ -23,7 +23,7 @@ do
     RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}"  http://127.0.0.1:${TARGET_PORT}/api/health)
 
     if [ ${RESPONSE_CODE} -eq 200 ]; then
-        echo "> New WAS successfully running"
+        echo "> New JAR successfully running"
         exit 0
     elif [ ${RETRY_COUNT} -eq 10 ]; then
         echo "> Health check failed."
