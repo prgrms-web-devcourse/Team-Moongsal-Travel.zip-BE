1d6c25f02a930d916d8015c16b8ec7dda6a0ceaa
cloudwi <cloudwi@naver.com>
2023-02-28 22:51:21 +0900

    🔨chore : ci/cd

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,39 +1,24 @@
 #!/bin/bash
+BUILD_WAR=$(ls /home/ec2-user/app/*.war)
+WAR_NAME=$(basename $BUILD_WAR)
+echo "## build 파일명: $WAR_NAME"
 
-REPOSITORY=/home/ec2-user/build
-PROJECT_NAME=springaws
+echo "## build 파일 복사"
+DEPLOY_PATH=/home/ec2-user/
+cp $BUILD_WAR $DEPLOY_PATH
 
-echo "> Build 파일 복사"
+CURRENT_PID=$(pgrep -f $WAR_NAME)
+echo "## 현재 실행중인 애플리케이션 pid: $CURRENT_PID"
 
-cp $REPOSITORY/zip/*.jar $REPOSITORY/
-
-echo "> 현재 구동중인 애플리케이션 pid 확인"
-
-CURRENT_PID=$(pgrep -fl springaws | grep jar | awk '{print $1}')
-
-echo "현재 구동중인 앱 pid: $CURRENT_PID"
-
-if [ -z "$CURRENT_PID" ]; then
-        echo "> 현재 구동 중인 앱이 없으므로 종료하지 않습니다."
+if [ -z $CURRENT_PID ]
+then
+  echo "## 현재 구동중인 애플리케이션 없음"
 else
-        echo "> kill -15 $CURRENT_PID"
-        kill -15 $CURRENT_PID
-        sleep 5
+  echo "## kill -15 $CURRENT_PID"
+  kill -15 $CURRENT_PID
+  sleep 5
 fi
 
-echo "> 새 애플리케이션 배포"
-
-JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
-
-echo "> JAR Name: $JAR_NAME"
-
-echo "> $JAR_NAME 에 실행권한 추가"
-
-chmod +x $JAR_NAME
-
-echo "> $JAR_NAME 실행"
-
-nohup java -jar \
-        -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties,classpath:/application-real.properties \
-        -Dspring.profiles.active=real \
-        $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+DEPLOY_WAR=$DEPLOY_PATH$WAR_NAME
+echo "## DEPLOY_WAR: $DEPLOY_WAR"
+nohup java -jar $DEPLOY_WAR  > $DEPLOY_PATH/nohup.out 2>&1 &
