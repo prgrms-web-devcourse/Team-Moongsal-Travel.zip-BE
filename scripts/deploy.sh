393c2e8b329b7c53bc4f771b1145855fdbb87a05
cloudwi <cloudwi@naver.com>
2023-03-01 16:06:30 +0900

    🔨chore : 환경변수 설정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,16 +5,29 @@ TARGET_PORT=0
 
 echo "> Current port of running JAR is ${CURRENT_PORT}."
 
-if [ ${CURRENT_PORT} -eq 8080 ]
+#if [ ${CURRENT_PORT} -eq 8080 ]
+#then
+#  TARGET_PORT=8081
+#elif [ ${CURRENT_PORT} -eq 8081 ]
+#then
+#  TARGET_PORT=8080
+#else
+#  echo "> No JAR is connected to nginx"
+#  TARGET_PORT=8080
+#fi
+
+#TARGET_PID=$(lsof -Fp -i TCP:${CURRENT_PORT} | grep -Po 'p[0-9]+' | grep -Po '[0-9]+')
+
+TARGET_PID=$(lsof -Fp -i TCP:8080 | grep -Po 'p[0-9]+' | grep -Po '[0-9]+')
+
+echo "$TARGET_PID"
+
+if [ ${TARGET_PID} -gt 0 ]
 then
-  TARGET_PORT=8081
-elif [ ${CURRENT_PORT} -eq 8081 ]
-then
-  TARGET_PORT=8080
-else
-  echo "> No JAR is connected to nginx"
-  TARGET_PORT=8080
+  echo "> Kill JAR running at ${CURRENT_PORT}."
+  sudo kill -9 ${TARGET_PID}
 fi
+
 
 #TARGET_PID=$(lsof -Fp -i TCP:${TARGET_PORT} | grep -Po 'p[0-9]+' | grep -Po '[0-9]+')
 
@@ -24,51 +37,42 @@ fi
 #  sudo kill -9 ${TARGET_PID}
 #fi
 
-nohup java -jar -Dserver.port=${TARGET_PORT} -Dspring.profiles.active=dev /home/ec2-user/travel-zip-deploy/build/libs/* &
+nohup java -jar -Dserver.port=8080 -Dspring.profiles.active=dev /home/ec2-user/travel-zip-deploy/build/libs/* &
 echo "> Now new JAR runs at ${TARGET_PORT}."
-
-echo "> Start health check of JAR at 'localhost:${TARGET_PORT}' ..."
-
-for RETRY_COUNT in 1 2 3 4 5 6 7 8 9 10
-do
-    echo "> #${RETRY_COUNT} trying..."
-    RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}"  localhost:${TARGET_PORT}/api/healths)
-
-    echo "RESPONSE_CODE ${RESPONSE_CODE}"
-
-    if [ ${RESPONSE_CODE} -eq 200 ]
-    then
-        echo "> New JAR successfully running"
-        exit 0
-    elif [ ${RETRY_COUNT} -eq 10 ]
-    then
-        echo "> Health check failed."
-        exit 1
-    fi
-    sleep 12
-done
-
-echo "> Nginx currently proxies to ${CURRENT_PORT}."
-
-# Change proxying port into target port
-echo "set \$service_url localhost:${TARGET_PORT};" | tee /home/ec2-user/service_url.inc
-
-echo "> Now Nginx proxies to ${TARGET_PORT}."
-
-# Reload nginx
-sudo service nginx reload
-
-echo "> Nginx reloaded."
-
-TARGET_PID=$(lsof -Fp -i TCP:${CURRENT_PORT} | grep -Po 'p[0-9]+' | grep -Po '[0-9]+')
-
-echo "$TARGET_PID"
-
-if [ ${TARGET_PID} -gt 0 ]
-then
-  echo "> Kill JAR running at ${CURRENT_PORT}."
-  sudo kill -9 ${TARGET_PID}
-fi
+#
+#echo "> Start health check of JAR at 'localhost:${TARGET_PORT}' ..."
+#
+#for RETRY_COUNT in 1 2 3 4 5 6 7 8 9 10
+#do
+#    echo "> #${RETRY_COUNT} trying..."
+#    RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}"  localhost:${TARGET_PORT}/api/healths)
+#
+#    echo "RESPONSE_CODE ${RESPONSE_CODE}"
+#
+#    if [ ${RESPONSE_CODE} -eq 200 ]
+#    then
+#        echo "> New JAR successfully running"
+#        exit 0
+#    elif [ ${RETRY_COUNT} -eq 10 ]
+#    then
+#        echo "> Health check failed."
+#        exit 1
+#    fi
+#    sleep 12
+#done
+#
+#echo "> Nginx currently proxies to ${CURRENT_PORT}."
+#
+## Change proxying port into target port
+#echo "set \$service_url localhost:${TARGET_PORT};" | tee /home/ec2-user/service_url.inc
+#
+#echo "> Now Nginx proxies to ${TARGET_PORT}."
+#
+## Reload nginx
+#sudo service nginx reload
+#
+#echo "> Nginx reloaded."
+#
 
 exit 0
 
