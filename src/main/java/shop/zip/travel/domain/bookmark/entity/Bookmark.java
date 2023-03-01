cb070cbf719465a81ce754ecd486355ed46b7726
yewon.seo <dodnjs1241@naver.com>
2023-03-01 11:36:26 +0900

    👨🏻‍💻feat: 북마크 엔티티 추가

---
+++ src/main/java/shop/zip/travel/domain/bookmark/entity/Bookmark.java
@@ -0,0 +1,52 @@
+package shop.zip.travel.domain.bookmark.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+import shop.zip.travel.domain.base.BaseTimeEntity;
+import shop.zip.travel.domain.member.entity.Member;
+import shop.zip.travel.domain.post.travelogue.entity.Travelogue;
+
+@Entity
+public class Bookmark extends BaseTimeEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne
+  @JoinColumn(name = "travelogue_id")
+  @OnDelete(action = OnDeleteAction.CASCADE)
+  private Travelogue travelogue;
+
+  @ManyToOne
+  @JoinColumn(name = "member_id")
+  @OnDelete(action = OnDeleteAction.CASCADE)
+  private Member member;
+
+  @Column(nullable = false)
+  private boolean status;
+
+  protected Bookmark() {
+  }
+
+  public Bookmark(Travelogue travelogue, Member member, boolean status) {
+    this.travelogue = travelogue;
+    this.member = member;
+    this.status = status;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+}
