9a87c42ff4d356761570035b75a1d288511ef64e
cloudwi <cloudwi@naver.com>
2023-03-02 00:10:20 +0900

    🚑fix :

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -52,6 +52,7 @@ do
     sleep 12
 done
 
+
 echo "> Nginx currently proxies to ${CURRENT_PORT}."
 
 echo "set \$service_url http://127.0.0.1:${TARGET_PORT};" |sudo tee /home/ec2-user/service_url.inc
