c06d5525364ae1ed0d0d8f9bed5d32c0af5c3944
Surin Lee <leesurin99@gmail.com>
2023-03-02 01:22:02 +0900

    👩🏻‍💻feat : 서브 게시물이 발행 가능한지 판단하는 서비스 레이어 생성
    - 각 서브 게시물이 발행 가능한 지 검증하는 서비스 구현

---
+++ src/main/java/shop/zip/travel/domain/post/subTravelogue/service/SubTraveloguePublishService.java
@@ -0,0 +1,36 @@
+package shop.zip.travel.domain.post.subTravelogue.service;
+
+import java.util.List;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import shop.zip.travel.domain.post.subTravelogue.data.Address;
+import shop.zip.travel.domain.post.subTravelogue.entity.SubTravelogue;
+import shop.zip.travel.domain.post.travelogue.exception.InvalidPublishTravelogueException;
+import shop.zip.travel.global.error.ErrorCode;
+
+@Service
+@Transactional(readOnly = true)
+public class SubTraveloguePublishService {
+
+  public void verifySubTravelogues(List<SubTravelogue> subTravelogues) {
+    subTravelogues.forEach(subTravelogue -> {
+      checkSubTravelogue(subTravelogue);
+      checkAddresses(subTravelogue.getAddresses());
+    });
+  }
+
+  private void checkSubTravelogue(SubTravelogue subTravelogue) {
+    if (subTravelogue.cannotPublish()) {
+      throw new InvalidPublishTravelogueException(ErrorCode.CANNOT_PUBLISH_TRAVELOGUE);
+    }
+  }
+
+  private void checkAddresses(List<Address> requestAddresses) {
+    requestAddresses.forEach(requestAddress -> {
+      if (requestAddress.cannotPublish()
+      ) {
+        throw new InvalidPublishTravelogueException(ErrorCode.CANNOT_PUBLISH_TRAVELOGUE);
+      }
+    });
+  }
+}
