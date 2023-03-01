3382365ec624d7b0cc8f51aeba1019e32a5e7af8
cloudwi <cloudwi@naver.com>
2023-03-01 19:35:03 +0900

    chore : 데이터베이스 오류로 인한 수정
    - profile local -> dev 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -26,7 +26,7 @@ fi
 #  sudo kill -9 ${TARGET_PID}
 #fi
 
-nohup java -jar -Dserver.port=${TARGET_PORT} /home/ec2-user/travel-zip-deploy/build/libs/* &
+nohup java -jar -Dserver.port=${TARGET_PORT} -Dspring.profiles.active=dev /home/ec2-user/travel-zip-deploy/build/libs/* &
 echo "> Now new JAR runs at ${TARGET_PORT}."
 
 echo "> Start health check of JAR at 'localhost:${TARGET_PORT}' ..."
