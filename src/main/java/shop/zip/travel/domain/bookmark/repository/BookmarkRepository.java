29edf8d3e455e4ca781ab4fded5bd43fc2513805
yewon.seo <dodnjs1241@naver.com>
2023-03-02 17:52:12 +0900

    👨🏻‍💻feat: bookmark id 가져오는 쿼리 추가

---
+++ src/main/java/shop/zip/travel/domain/bookmark/repository/BookmarkRepository.java
@@ -0,0 +1,19 @@
+package shop.zip.travel.domain.bookmark.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import shop.zip.travel.domain.bookmark.entity.Bookmark;
+import shop.zip.travel.domain.bookmark.repository.querydsl.BookmarkRepositoryQuerydsl;
+
+public interface BookmarkRepository extends JpaRepository<Bookmark, Long>,
+    BookmarkRepositoryQuerydsl {
+
+  @Query("select b.id "
+      + "from Bookmark b "
+      + "where b.member.id = :memberId "
+      + "and  b.travelogue.id = :travelogueId")
+  Long getBookmarkId(@Param("memberId") Long memberId,
+      @Param("travelogueId") Long travelogueId);
+
+}
