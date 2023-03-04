c5077831369b1fc707c10d538bdff9f9869921d8
yewon.seo <dodnjs1241@naver.com>
2023-03-04 21:18:06 +0900

    ♻️refactor: 쿠키 생성 로직 수정

--- src/main/java/shop/zip/travel/domain/post/util/CookieUtil.java
+++ src/main/java/shop/zip/travel/domain/post/util/CookieUtil.java
@@ -8,13 +8,13 @@ import org.springframework.web.util.WebUtils;
 
 public class CookieUtil {
 
-  public static final String VIEW_COUNT = "viewCount";
+  public static final String VIEW_COUNT = "viewCountCookie_";
   public static final int DAY = 60 * 60 * 24;
 
   public static boolean canAddViewCount(HttpServletRequest request, HttpServletResponse response,
       Long travelogueId) {
     if (hasViewCountCookie(request, travelogueId)) {
-      Cookie cookie = new Cookie(VIEW_COUNT, String.valueOf(travelogueId));
+      Cookie cookie = new Cookie(VIEW_COUNT + travelogueId, String.valueOf(travelogueId));
       cookie.setMaxAge(DAY);
       response.addCookie(cookie);
       return true;
@@ -24,7 +24,7 @@ public class CookieUtil {
 
   private static boolean hasViewCountCookie(HttpServletRequest request, Long travelogueId) {
 
-    Cookie cookie = WebUtils.getCookie(request, VIEW_COUNT);
+    Cookie cookie = WebUtils.getCookie(request, VIEW_COUNT + travelogueId);
 
     if (Objects.isNull(cookie)) {
       return true;
