2aeeb64373c08c535ba7ba0f738477126f70f19e
cloudwi <cloudwi@naver.com>
2023-03-04 13:10:25 +0900

    fix : cors 에러 수정

--- src/main/java/shop/zip/travel/global/config/SecurityConfig.java
+++ src/main/java/shop/zip/travel/global/config/SecurityConfig.java
@@ -2,13 +2,11 @@ package shop.zip.travel.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.oauth2.client.web.OAuth2LoginAuthenticationFilter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -28,8 +26,7 @@ public class SecurityConfig {
 
   @Bean
   public WebSecurityCustomizer webSecurityCustomizer() {
-    return web -> web.ignoring()
-        .requestMatchers("/**");
+    return web -> web.ignoring();
   }
 
   @Bean
