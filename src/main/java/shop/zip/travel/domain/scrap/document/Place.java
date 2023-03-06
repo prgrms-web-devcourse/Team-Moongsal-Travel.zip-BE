b85a2efe15dc26e7627952c0a2fe9c1c7b1f4c93
Albatross3 <superstring77@gmail.com>
2023-03-07 06:14:19 +0900

    🧑🏻‍💻feat : Place 필드 추가

--- src/main/java/shop/zip/travel/domain/scrap/document/Place.java
+++ src/main/java/shop/zip/travel/domain/scrap/document/Place.java
@@ -1,15 +1,31 @@
 package shop.zip.travel.domain.scrap.document;
 
 
+import jakarta.persistence.Id;
+import org.bson.types.ObjectId;
+
 public class Place {
+
+  @Id
+  String objectId;
 
   String placeName;
 
   public Place(String placeName) {
+    this.objectId = ObjectId.get().toString();
     this.placeName = placeName;
   }
 
   protected Place() {
 
   }
+
+  public String getObjectId() {
+    return objectId;
+  }
+
+  public String getPlaceName() {
+    return placeName;
+  }
+
 }
