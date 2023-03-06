52647971263153b89ef181de8e28bbb5f9bc304e
yewon.seo <dodnjs1241@naver.com>
2023-03-06 19:06:49 +0900

    ♻️refactor: 쿠키 메서드명 수정 및 기본 생성자 추가
    -hasViewCountCookie 메서드 명
    hasNotViewCountCookie 로 변경

--- src/main/java/shop/zip/travel/domain/post/util/CookieUtil.java
+++ src/main/java/shop/zip/travel/domain/post/util/CookieUtil.java
@@ -11,9 +11,12 @@ public class CookieUtil {
   public static final String VIEW_COUNT = "viewCountCookie_";
   public static final int DAY = 60 * 60 * 24;
 
+  private CookieUtil() {
+  }
+
   public static boolean canAddViewCount(HttpServletRequest request, HttpServletResponse response,
       Long travelogueId) {
-    if (hasViewCountCookie(request, travelogueId)) {
+    if (hasNotViewCountCookie(request, travelogueId)) {
       Cookie cookie = new Cookie(VIEW_COUNT + travelogueId, String.valueOf(travelogueId));
       cookie.setMaxAge(DAY);
       response.addCookie(cookie);
@@ -22,7 +25,7 @@ public class CookieUtil {
     return false;
   }
 
-  private static boolean hasViewCountCookie(HttpServletRequest request, Long travelogueId) {
+  private static boolean hasNotViewCountCookie(HttpServletRequest request, Long travelogueId) {
 
     Cookie cookie = WebUtils.getCookie(request, VIEW_COUNT + travelogueId);
 
