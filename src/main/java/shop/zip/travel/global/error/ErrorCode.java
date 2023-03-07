762af8e3231cba067f3125a8de054da2c4ba3ae1
Surin Lee <leesurin99@gmail.com>
2023-03-07 13:45:50 +0900

    👩🏻‍💻feat: 게시물 수정 관련 예외 추가

--- src/main/java/shop/zip/travel/global/error/ErrorCode.java
+++ src/main/java/shop/zip/travel/global/error/ErrorCode.java
@@ -25,7 +25,11 @@ public enum ErrorCode {
 	TRAVELOGUE_NOT_FOUND(HttpStatus.NOT_FOUND, "여행기를 찾을 수 없습니다"),
 	NO_AUTHORIZATION_TO_TRAVELOGUE(HttpStatus.UNAUTHORIZED, "접근권한이 없습니다."),
 	CANNOT_PUBLISH_TRAVELOGUE(HttpStatus.BAD_REQUEST, "아직 작성이 완료되지 않은 페이지가 있기 때문에 공개할 수 없습니다."),
-	NOT_LOGGED_IN(HttpStatus.UNAUTHORIZED, "로그인후 이용이 가능합니다");
+	NOT_LOGGED_IN(HttpStatus.UNAUTHORIZED, "로그인후 이용이 가능합니다"),
+
+	// subTravelogue
+	SUB_TRAVELOGUE_NOT_FOUND(HttpStatus.NOT_FOUND, "게시물을 찾을 수 없습니다."),
+	TRAVELOGUE_NOT_CONTAIN_SUB_TRAVELOGUE(HttpStatus.BAD_REQUEST, "잘못된 접근입니다.");
 
 
 	private final HttpStatus status;
