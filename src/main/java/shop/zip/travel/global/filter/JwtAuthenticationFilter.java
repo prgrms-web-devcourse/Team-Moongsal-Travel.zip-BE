137721dd01dffbd1431efec94d09a9a2eb8c6d4a
cloudwi <cloudwi@naver.com>
2023-03-09 02:24:31 +0900

    🚑 fix : 토큰 유효성 검사 시 http code 내려보내기

--- src/main/java/shop/zip/travel/global/filter/JwtAuthenticationFilter.java
+++ src/main/java/shop/zip/travel/global/filter/JwtAuthenticationFilter.java
@@ -6,6 +6,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -37,7 +38,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         SecurityContextHolder.getContext().setAuthentication(authentication);
       }
     } catch (BusinessException e) {
-      response.setStatus(e.getErrorCode().getStatusValue());
+      response.setStatus(HttpStatus.UNPROCESSABLE_ENTITY.value());
       response.setContentType(MediaType.APPLICATION_JSON_VALUE);
       response.setCharacterEncoding("UTF-8");
       objectMapper.writeValue(response.getWriter(),
