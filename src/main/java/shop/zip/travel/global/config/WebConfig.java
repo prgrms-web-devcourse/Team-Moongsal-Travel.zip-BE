ac3d6555bc2eeea123e88f70910db3c8cddb07cd
Albatross3 <superstring77@gmail.com>
2023-03-10 18:38:40 +0900

    ♻️refactor : error 수정

--- src/main/java/shop/zip/travel/global/config/WebConfig.java
+++ src/main/java/shop/zip/travel/global/config/WebConfig.java
@@ -14,7 +14,6 @@ public class WebConfig implements WebMvcConfigurer {
   public void addCorsMappings(CorsRegistry corsRegistry) {
     corsRegistry.addMapping("/**")
         .allowedOrigins("http://localhost:3000","https://travel-zip.vercel.app")
-        .allowedOrigins("*")
         .allowedMethods("GET","POST","PUT","PATCH","DELETE","OPTIONS","HEAD")
         .allowedHeaders("*")
         .allowCredentials(true)
