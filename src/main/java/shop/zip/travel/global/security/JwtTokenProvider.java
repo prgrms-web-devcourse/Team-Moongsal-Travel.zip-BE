decd21ebc87b82ddc09b4fa26f9cc0b3ab12657a
cloudwi <cloudwi@naver.com>
2023-03-12 02:05:06 +0900

    🔨chore : ACCESS_TOKEN_EXPIRED_TIME 증가

--- src/main/java/shop/zip/travel/global/security/JwtTokenProvider.java
+++ src/main/java/shop/zip/travel/global/security/JwtTokenProvider.java
@@ -32,7 +32,7 @@ public class JwtTokenProvider {
   private final String accessTokenSecretKey;
   private final String refreshTokenSecretKey;
 
-  private final long ACCESS_TOKEN_EXPIRED_TIME = Duration.ofMinutes(5).toMillis();
+  private final long ACCESS_TOKEN_EXPIRED_TIME = Duration.ofDays(7).toMillis();
   private final long REFRESH_TOKEN_EXPIRED_TIME = Duration.ofHours(2).toMillis();
 
   private final CustomUserDetailsService customUserDetailsService;
