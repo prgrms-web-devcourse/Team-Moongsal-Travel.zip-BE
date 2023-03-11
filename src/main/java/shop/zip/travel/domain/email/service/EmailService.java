2fcf6d0585a6c92093ba2a2f949fced8797241b7
cloudwi <cloudwi@naver.com>
2023-03-12 01:37:11 +0900

    ♻️refactor : toSubTravelogueUpdate 수정

--- src/main/java/shop/zip/travel/domain/email/service/EmailService.java
+++ src/main/java/shop/zip/travel/domain/email/service/EmailService.java
@@ -5,10 +5,7 @@ import jakarta.mail.internet.InternetAddress;
 import jakarta.mail.internet.MimeMessage;
 import jakarta.mail.internet.MimeMessage.RecipientType;
 import java.io.UnsupportedEncodingException;
-import java.text.MessageFormat;
 import java.util.Random;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 import shop.zip.travel.domain.email.exception.SendEmailException;
@@ -17,8 +14,6 @@ import shop.zip.travel.global.util.RedisUtil;
 
 @Service
 public class EmailService {
-
-  private final Logger log = LoggerFactory.getLogger(EmailService.class);
 
   private final JavaMailSender javaMailSender;
   private final RedisUtil redisUtil;
