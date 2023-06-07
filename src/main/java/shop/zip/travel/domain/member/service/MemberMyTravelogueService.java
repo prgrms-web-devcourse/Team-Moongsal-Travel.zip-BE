7217f38d84138fb7dfb0aa1eac57744bfb044c9d
Surin Lee <leesurin99@gmail.com>
2023-06-08 01:01:08 +0900

    ♻️refactor: 자신이 작성한 글 목록 가져오는 API 외의 메소드 분리

--- src/main/java/shop/zip/travel/domain/member/service/MemberMyTravelogueService.java
+++ src/main/java/shop/zip/travel/domain/member/service/MemberMyTravelogueService.java
@@ -4,37 +4,25 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import shop.zip.travel.domain.post.subTravelogue.dto.req.SubTravelogueUpdateReq;
-import shop.zip.travel.domain.post.subTravelogue.dto.res.SubTravelogueDetailRes;
-import shop.zip.travel.domain.post.subTravelogue.dto.res.SubTravelogueUpdateRes;
-import shop.zip.travel.domain.post.subTravelogue.entity.SubTravelogue;
-import shop.zip.travel.domain.post.subTravelogue.exception.SubTravelogueNotFoundException;
-import shop.zip.travel.domain.post.subTravelogue.repository.SubTravelogueRepository;
+import shop.zip.travel.domain.post.travelogue.dto.TempTravelogueSimple;
 import shop.zip.travel.domain.post.travelogue.dto.TravelogueSimple;
-import shop.zip.travel.domain.post.travelogue.dto.TravelogueUpdate;
-import shop.zip.travel.domain.post.travelogue.dto.req.TravelogueUpdateReq;
+import shop.zip.travel.domain.post.travelogue.dto.res.TempTravelogueSimpleRes;
 import shop.zip.travel.domain.post.travelogue.dto.res.TravelogueCustomSlice;
-import shop.zip.travel.domain.post.travelogue.dto.res.TravelogueDetailForUpdateRes;
 import shop.zip.travel.domain.post.travelogue.dto.res.TravelogueSimpleRes;
-import shop.zip.travel.domain.post.travelogue.dto.res.TravelogueUpdateRes;
-import shop.zip.travel.domain.post.travelogue.entity.Travelogue;
-import shop.zip.travel.domain.post.travelogue.exception.TravelogueNotFoundException;
 import shop.zip.travel.domain.post.travelogue.repository.TravelogueRepository;
-import shop.zip.travel.global.error.ErrorCode;
 
 @Service
 @Transactional(readOnly = true)
 public class MemberMyTravelogueService {
 
   private static final boolean PUBLISHED = true;
+  private static final boolean TEMP = false;
 
   private final TravelogueRepository travelogueRepository;
-  private final SubTravelogueRepository subTravelogueRepository;
 
-  public MemberMyTravelogueService(TravelogueRepository travelogueRepository,
-      SubTravelogueRepository subTravelogueRepository) {
+
+  public MemberMyTravelogueService(TravelogueRepository travelogueRepository) {
     this.travelogueRepository = travelogueRepository;
-    this.subTravelogueRepository = subTravelogueRepository;
   }
 
   public TravelogueCustomSlice<TravelogueSimpleRes> getTravelogues(Long memberId,
@@ -46,58 +34,14 @@ public class MemberMyTravelogueService {
     return TravelogueCustomSlice.toDto(myTravelogues.map(TravelogueSimpleRes::toDto));
   }
 
-  public TravelogueDetailForUpdateRes getTravelogueForUpdate(Long memberId, Long travelogueId) {
-    Travelogue travelogue = getMyTravelogue(travelogueId, memberId);
+  public TravelogueCustomSlice<TempTravelogueSimpleRes> getMyTempTravelogues(Long memberId,
+      Pageable pageable) {
+    Slice<TempTravelogueSimple> myTempTravelogues = travelogueRepository.getMyTempTravelogues(
+        memberId, pageable, TEMP);
 
-    return TravelogueDetailForUpdateRes.toDto(travelogue);
+    return TravelogueCustomSlice.toDto(
+        myTempTravelogues.map(TempTravelogueSimpleRes::toDto)
+    );
   }
 
-  @Transactional
-  public TravelogueUpdateRes updateTravelogue(Long travelogueId, Long memberId,
-      TravelogueUpdateReq travelogueUpdateReq) {
-    Travelogue travelogue = getMyTravelogue(travelogueId, memberId);
-    TravelogueUpdate travelogueUpdate = travelogueUpdateReq.toTravelogueUpdate();
-
-    travelogue.update(travelogueUpdate);
-
-    return TravelogueUpdateRes.toDto(travelogue.getId());
-  }
-
-  public SubTravelogueDetailRes getSubTravelogueForUpdate(Long memberId, Long travelogueId,
-      Long subTravelogueId) {
-    Travelogue travelogue = getMyTravelogue(travelogueId, memberId);
-    SubTravelogue subTravelogue = getSubTravelogue(subTravelogueId);
-    travelogue.isContain(subTravelogue);
-
-    return SubTravelogueDetailRes.toDto(subTravelogue);
-  }
-
-  private Travelogue getMyTravelogue(Long travelogueId, Long memberId) {
-    Travelogue travelogue = travelogueRepository.findById(travelogueId)
-        .orElseThrow(() -> new TravelogueNotFoundException(ErrorCode.TRAVELOGUE_NOT_FOUND));
-    travelogue.validWriter(memberId);
-    return travelogue;
-  }
-
-  private SubTravelogue getSubTravelogue(Long subTravelogueId) {
-    return subTravelogueRepository.findById(subTravelogueId)
-        .orElseThrow(() -> new SubTravelogueNotFoundException(ErrorCode.SUB_TRAVELOGUE_NOT_FOUND));
-  }
-
-  @Transactional
-  public SubTravelogueUpdateRes updateSubTravelogue(
-      Long travelogueId,
-      Long subTravelogueId,
-      Long memberId,
-      SubTravelogueUpdateReq subTravelogueUpdateReq) {
-    Travelogue travelogue = getMyTravelogue(travelogueId, memberId);
-    SubTravelogue subTravelogue = getSubTravelogue(subTravelogueId);
-    travelogue.isContain(subTravelogue);
-
-    subTravelogue.update(subTravelogueUpdateReq.toSubTravelogueUpdate());
-
-    travelogue.updateSubTravelogues(subTravelogue);
-
-    return SubTravelogueUpdateRes.toDto(subTravelogue.getId());
-  }
 }
