debfd0c9bc35934cf26b6941124049abcde64964
yewon.seo <dodnjs1241@naver.com>
2023-06-10 01:24:39 +0900

    ♻️refactor : 양방향 매핑으로 인한 stackOverFlow해결
    - jpa Object 를 redis(저장방법 String)에 저장시 toString호출로 인한 stackOverFlow문제 해결

--- src/main/java/shop/zip/travel/presentation/suggestion/SuggestionController.java
+++ src/main/java/shop/zip/travel/presentation/suggestion/SuggestionController.java
@@ -6,7 +6,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import shop.zip.travel.domain.post.travelogue.dto.res.TravelogueCustomSlice;
 import shop.zip.travel.domain.post.travelogue.dto.res.TravelogueSimpleRes;
