87a6a9328eca41100c28616f1eb8fe1a4ec43de2
yewon.seo <dodnjs1241@naver.com>
2023-06-11 13:08:58 +0900

    👨🏻‍💻feat: 안쓰는 Import제거, getCountryName() 추가

--- src/main/java/shop/zip/travel/domain/suggestion/entity/Suggestion.java
+++ src/main/java/shop/zip/travel/domain/suggestion/entity/Suggestion.java
@@ -1,13 +1,8 @@
 package shop.zip.travel.domain.suggestion.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
 import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.index.Indexed;
-import shop.zip.travel.domain.base.BaseTimeEntity;
-import shop.zip.travel.domain.member.entity.Member;
 import shop.zip.travel.domain.post.travelogue.entity.Travelogue;
 
 @RedisHash(timeToLive = 60 * 60 * 24 * 7)
@@ -35,5 +30,9 @@ public class Suggestion {
 
   public Long getMemberId() {
     return memberId;
+  }
+
+  public String getCountryName() {
+    return this.travelogue.getCountry().getName();
   }
 }
