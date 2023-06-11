f4f12c3c098dfc9a5f105d56042b71659309a83a
yewon.seo <dodnjs1241@naver.com>
2023-06-11 13:09:33 +0900

    ♻️refactor: travelogue service와 SuggestionService 코드 분리

--- src/main/java/shop/zip/travel/domain/suggestion/service/SuggestionService.java
+++ src/main/java/shop/zip/travel/domain/suggestion/service/SuggestionService.java
@@ -1,21 +1,12 @@
 package shop.zip.travel.domain.suggestion.service;
 
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import static java.util.stream.Collectors.groupingBy;
+
 import java.util.Map.Entry;
-import java.util.concurrent.atomic.AtomicInteger;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.data.domain.Pageable;
+import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import shop.zip.travel.domain.post.travelogue.dto.res.TravelogueCustomSlice;
-import shop.zip.travel.domain.post.travelogue.dto.res.TravelogueSimpleRes;
 import shop.zip.travel.domain.post.travelogue.entity.Travelogue;
-import shop.zip.travel.domain.post.travelogue.service.TravelogueService;
 import shop.zip.travel.domain.suggestion.entity.Suggestion;
 import shop.zip.travel.domain.suggestion.repository.SuggestionRepository;
 
@@ -23,15 +14,12 @@ import shop.zip.travel.domain.suggestion.repository.SuggestionRepository;
 @Transactional(readOnly = true)
 public class SuggestionService {
 
-  private final Logger log = LoggerFactory.getLogger(Suggestion.class);
+  private static final String DEFAULT_COUNTRY = "대한민국";
 
   private final SuggestionRepository suggestionRepository;
-  private final TravelogueService travelogueService;
 
-  public SuggestionService(SuggestionRepository suggestionRepository,
-      TravelogueService travelogueService) {
+  public SuggestionService(SuggestionRepository suggestionRepository) {
     this.suggestionRepository = suggestionRepository;
-    this.travelogueService = travelogueService;
   }
 
   @Transactional
@@ -41,29 +29,14 @@ public class SuggestionService {
     suggestionRepository.save(suggestion);
   }
 
-  @Transactional
-  public TravelogueCustomSlice<TravelogueSimpleRes> findByMemberId(Long memberId, Pageable pageable) {
-    List<Suggestion> suggestions = suggestionRepository.getSuggestionByMemberId(memberId);
-    Map<String, Long> map = new HashMap<>();
-
-    map.put("대한민국", 1L);
-
-    suggestions.forEach(suggestion-> {
-      System.out.println("suggestions 만들어짐 ");
-      String keyword = suggestion.getTravelogue().getCountry().getName();
-      map.put(keyword, map.getOrDefault(keyword, 0L) + 1);
-    });
-
-    Comparator<Entry<String, Long>> comparator = new Comparator<Entry<String, Long>>() {
-      @Override
-      public int compare(Entry<String, Long> e1, Entry<String, Long> e2) {
-        return e1.getValue().compareTo(e2.getValue());
-      }
-    };
-
-    Entry<String, Long> maxEntry = Collections.max(map.entrySet(), comparator);
-    log.error("maxEntry.getValue() : " + maxEntry.getValue());
-
-    return travelogueService.search(maxEntry.getKey(), pageable);
+  public String getTopSuggestionCountry(Long memberId) {
+    return suggestionRepository.getSuggestionByMemberId(memberId)
+        .stream()
+        .collect(groupingBy(Suggestion::getCountryName, Collectors.counting()))
+        .entrySet()
+        .stream()
+        .max(Entry.comparingByValue())
+        .map(Entry::getKey)
+        .orElse(DEFAULT_COUNTRY);
   }
 }
--- src/main/java/shop/zip/travel/presentation/suggestion/SuggestionController.java
+++ src/main/java/shop/zip/travel/presentation/suggestion/SuggestionController.java
@@ -6,10 +6,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import shop.zip.travel.domain.post.travelogue.dto.res.TravelogueCustomSlice;
 import shop.zip.travel.domain.post.travelogue.dto.res.TravelogueSimpleRes;
+import shop.zip.travel.domain.post.travelogue.service.TravelogueService;
 import shop.zip.travel.domain.suggestion.service.SuggestionService;
 import shop.zip.travel.global.security.UserPrincipal;
 
@@ -20,9 +20,12 @@ public class SuggestionController {
   private static final int DEFAULT_SIZE = 5;
 
   private final SuggestionService suggestionService;
+  private final TravelogueService travelogueService;
 
-  public SuggestionController(SuggestionService suggestionService) {
+  public SuggestionController(SuggestionService suggestionService,
+      TravelogueService travelogueService) {
     this.suggestionService = suggestionService;
+    this.travelogueService = travelogueService;
   }
 
   @GetMapping
@@ -30,10 +33,11 @@ public class SuggestionController {
       @AuthenticationPrincipal UserPrincipal userPrincipal,
       @PageableDefault(size = DEFAULT_SIZE) Pageable pageable) {
 
-    TravelogueCustomSlice<TravelogueSimpleRes> travelogueSimpleResList = suggestionService.findByMemberId(
-        userPrincipal.getUserId(), pageable);
+    String suggestionCountry = suggestionService.getTopSuggestionCountry(userPrincipal.getUserId());
+    TravelogueCustomSlice<TravelogueSimpleRes> result = travelogueService.search(suggestionCountry,
+        pageable);
 
-    return ResponseEntity.ok(travelogueSimpleResList);
+    return ResponseEntity.ok(result);
   }
 
 }
